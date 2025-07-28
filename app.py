2700af0aee83e253dc94bb525303d0895a41697e
Bijay Khatiwada <bijaymstry@gmail.com>
2025-07-28 15:37:22 +0100

    disable certificate validation temporarily

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 CORS(app)
 
-client = MongoClient("mongodb+srv://bijaymstry:Ob0hqn0hidUbPhrM@cluster0.qewaugl.mongodb.net/bizDB?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://<user>:<pass>@cluster0.mongodb.net/?retryWrites=true&w=majority&ssl=true&tls=true&tlsAllowInvalidCertificates=true&tlsInsecure=true")
+
 
 db = client.bizDB # select the database
 tasks = db.tasks_data # selects the collection 
